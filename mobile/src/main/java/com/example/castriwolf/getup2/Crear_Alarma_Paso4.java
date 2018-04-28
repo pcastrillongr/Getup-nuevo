6a76e0a6c2f10048d85aa00f88162c85d83cb7f9
pablocastrillon <cristinavilas@192.168.1.33>
2018-04-28 20:45:59 +0200

    Activty Tiempo Desayuno sin fucnionalidad

--- mobile/src/main/java/com/example/castriwolf/getup2/Crear_Alarma_Paso4.java
+++ mobile/src/main/java/com/example/castriwolf/getup2/Crear_Alarma_Paso4.java
@@ -1,13 +1,16 @@
 package com.example.castriwolf.getup2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.NumberPicker;
 
 public class Crear_Alarma_Paso4 extends AppCompatActivity {
 
     NumberPicker picker;
-
+    ImageView next;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,5 +18,16 @@ public class Crear_Alarma_Paso4 extends AppCompatActivity {
         picker=(NumberPicker)findViewById(R.id.numberpicker);
         picker.setMaxValue(59);
         picker.setMinValue(0);
+        next=(ImageView)findViewById(R.id.next);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent go=new Intent(getApplicationContext(),Crear_Alarma_Paso5.class);
+                startActivity(go);
+            }
+        });
+
+
     }
 }
