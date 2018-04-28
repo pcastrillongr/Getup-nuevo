eb213501e2b95b99a0bf0e6b923ce6f1f600811f
pablocastrillon <cristinavilas@192.168.1.33>
2018-04-28 19:57:38 +0200

    Mueve la camara con zoom a el primer punto desde donde se dibuja la linea de distancia

---
+++ mobile/src/main/java/com/example/castriwolf/getup2/Crear_Alarma_Paso1.java
@@ -0,0 +1,32 @@
+package com.example.castriwolf.getup2;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.NumberPicker;
+
+public class Crear_Alarma_Paso1 extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crear_alarma_paso1);
+
+
+
+
+    }
+
+
+    public void tiempo(View view){
+
+    }
+
+    public void distancia(View view){
+
+
+    }
+}
